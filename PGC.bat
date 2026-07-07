06033712908d99f117748e4f9d2b7591ce16f1fb
Kaua-da-Silva-Padin <kauapadin5@gmail.com>
2026-07-07 17:23:16 -0300

    Deleted PGC

--- PGC.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set /p git_repo="Enter the repository name (without the .git): "
-git remote set-url origin git@github.com:Kaua-da-Silva-Padin/%git_repo%.git
-set /p current_branch="Enter the current branch: "
-git push -u origin %current_branch%
-
-echo "Your problem should be solved now, thanks for using P.G.C (Punch Git Cat)!"
-pause
